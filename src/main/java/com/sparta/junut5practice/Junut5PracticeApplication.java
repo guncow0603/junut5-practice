e7d7dc29b8e3ae6bb8d090b1878056ba79007f8d
guncow0603 <guncow0603@gmail.com>
2023-11-29 15:26:02 +0900

    init commit

---
+++ src/main/java/com/sparta/junut5practice/Junut5PracticeApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.junut5practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junut5PracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Junut5PracticeApplication.class, args);
+	}
+
+}
